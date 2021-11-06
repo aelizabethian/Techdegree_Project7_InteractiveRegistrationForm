1fc6a829b4342a615e9364b22a673baf7914fb77
aelizabethian <amanda.richardson2@gmail.com>
2021-11-06 14:58:04 -0400

    Adding funcitonality
    Continuing to finish the assignment, including doing the exceeds expectations elements.

--- js/script.js
+++ js/script.js
@@ -62,6 +62,8 @@ activities.addEventListener("change", (e) => {
   totalDisplay.innerHTML = `Total: $${total}`;
 });
 
+//Prevent users from registering for multiple activities that conflict
+
 //_________________________________________
 //Setting the payment systems so that the preferred is displayed, and the rest hidden
 
@@ -203,3 +205,125 @@ for (let i = 0; i < indActivity.length; i++) {
     indActivity[i].parentNode.classList.remove("focus");
   });
 }
+
+//Preventing users from signing up on activites at the same day/time
+
+for (let i = 0; i < indActivity.length; i++) {
+  indActivity[i].addEventListener("click", (e) => {
+    let target = e.target;
+    let dataTime = target.getAttribute("data-day-and-time");
+
+    if (target.checked) {
+      for (let j = 0; j < indActivity.length; j++) {
+        let attribute = indActivity[j].getAttribute("data-day-and-time");
+
+        if (attribute === dataTime) {
+          indActivity[j].disabled = true;
+          indActivity[j].parentNode.classList.add("disabled");
+          target.disabled = false;
+          target.parentNode.classList.remove("disabled");
+        }
+      }
+    }
+    if (!target.checked) {
+      for (let j = 0; j < indActivity.length; j++) {
+        let attribute = indActivity[j].getAttribute("data-day-and-time");
+        if (attribute === dataTime) {
+          indActivity[j].disabled = false;
+          indActivity[j].parentNode.classList.remove("disabled");
+        }
+      }
+    }
+  });
+}
+
+//--------
+// Providing real time error message
+
+//First in email - create a new span error message to display or hide depending on message
+const input = document.createElement("span");
+input.innerHTML = "Email must contain an @ symbol";
+email.parentNode.append(input);
+input.style.display = "none";
+input.style.color = "red";
+
+email.addEventListener("keyup", () => {
+  const emailValue = email.value;
+  const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
+
+  if (!emailIsValid) {
+    if (!emailValue.includes("@")) {
+      input.style.display = "block";
+    }
+
+    //Removes the error when the user includes the @ symbol
+    if (emailValue.includes("@")) {
+      input.style.display = "none";
+    }
+  }
+
+  if (emailIsValid) {
+    input.style.display = "none";
+  }
+});
+
+//Also for credit card to just check that they entered at least a 13-digit number
+const inputCredit = document.createElement("span");
+inputCredit.innerHTML = "Entry must be at least 13 characters long";
+
+const inputCreditOther = document.createElement("span");
+inputCreditOther.innerHTML = "Entries must be in numerical format only";
+
+creditCardNo.parentNode.append(inputCredit);
+inputCredit.style.display = "none";
+inputCredit.style.color = "red";
+
+creditCardNo.parentNode.append(inputCreditOther);
+inputCreditOther.style.display = "none";
+inputCreditOther.style.color = "red";
+
+creditCardNo.addEventListener("keyup", () => {
+  const creditCardNoValue = creditCardNo.value;
+  let creditCardNoValueNoSpace = creditCardNoValue.replace(/ /g, "").trim();
+
+  //This regex from https://gist.github.com/hto/060a1d3df81929eedd51799b160b79c8
+  const creditIsValid =
+    /4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11}$/.test(
+      creditCardNoValueNoSpace
+    );
+
+  const creditContainsLetters = /[A-Za-z]/.test(creditCardNoValueNoSpace);
+
+  if (!creditIsValid) {
+    inputCredit.style.display = "none";
+    inputCreditOther.style.display = "none";
+
+    if (creditContainsLetters) {
+      //This first one is to reset the error so that two errors aren't present at the same time. The character one takes precedence here.
+      // inputCredit.style.display = "none";
+      inputCreditOther.style.display = "block";
+    }
+  }
+
+  /*Adding a condition here that only shows up is the other condition isn't met so there aren't two error messages shown .Also adding a length property so that
+  if the person includes characters and resets the field, this error doesn't show up immediately on a blank input field */
+
+  if (
+    !creditIsValid &&
+    creditContainsLetters !== true &&
+    creditCardNoValue.length >= 2
+  ) {
+    inputCreditOther.style.display = "none";
+    if (creditCardNoValueNoSpace.indexOf(13) === -1) {
+      inputCredit.style.display = "block";
+    }
+  }
+  //Adding this to make sure that the error disappears once they put in the appropriate letters/cases
+  if (creditCardNoValueNoSpace.length >= 13) {
+    inputCredit.style.display = "none";
+  }
+
+  if (creditIsValid) {
+    inputCreditOther.style.display = "none";
+  }
+});
